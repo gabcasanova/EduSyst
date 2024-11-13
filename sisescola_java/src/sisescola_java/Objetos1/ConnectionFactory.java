373f00a23d1d3c292e6db4e6525c2efb32b60157
gabcasanova <con.casanovaproductions@outlook.com>
2024-11-13 14:21:26 -0300

    classes atualizadas

--- sisescola_java/src/sisescola_java/Objetos1/ConnectionFactory.java
+++ sisescola_java/src/sisescola_java/Objetos1/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
     private static final String DRIVER =  "com.mysql.cj.jdbc.Driver";
     private static final String url = "jdbc:mysql://localhost:3306/edusyst";
     private static final String user = "root";
-    private static final String password = "";
+    private static final String password = "root";
     private static Connection con;
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
